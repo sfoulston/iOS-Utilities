482311225a0c1d5522b0edc2eede5cdbe69e72cd
Stuart Foulston <sfoulston@me.com>
2018-05-21 22:06:20 +0100

    Initial commit

---
+++ iOS Utilities/iOS_Utilities.h
@@ -0,0 +1,19 @@
+//
+//  iOS_Utilities.h
+//  iOS Utilities
+//
+//  Created by Stuart Foulston on 21/05/2018.
+//  Copyright © 2018 Stuart Foulston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOS_Utilities.
+FOUNDATION_EXPORT double iOS_UtilitiesVersionNumber;
+
+//! Project version string for iOS_Utilities.
+FOUNDATION_EXPORT const unsigned char iOS_UtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_Utilities/PublicHeader.h>
+
+
